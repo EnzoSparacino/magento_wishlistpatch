fc9075b2f80f85e11a4f595214f9b96b5594487e
EnzoSparacino <esparacino@gmail.com>
2018-07-21 13:12:37 +0200

    first commit

---
+++ app/code/community/Sparacino/WishlistPatch/Helper/Data.php
@@ -0,0 +1,5 @@
+<?php
+class Sparacino_WishlistPatch_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
+	 
---
+++ app/code/community/Sparacino/WishlistPatch/Model/Observer.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ *
+ * @author     Enzo Franco Sparacino <esparacino@gmail.com>
+ * 
+ */
+
+?>
+
+<?php
+class Sparacino_WishlistPatch_Model_Observer
+{
+
+	public function replaceFormKeyForWishlistAdd(Varien_Event_Observer $observer)
+	{
+		$formKey = Mage::getSingleton('core/session')->getFormKey();
+
+		$session = Mage::getSingleton('customer/session');
+		$beforeWishlistRequest = $session->getBeforeWishlistRequest();
+
+		$beforeWishlistRequest['form_key'] = $formKey;
+		$session->setBeforeWishlistRequest($beforeWishlistRequest);
+
+		$newBeforeAuthUrl = Mage::getUrl('wishlist/index/add',
+			array('product' => $beforeWishlistRequest['product'], 'form_key' => $formKey)
+		);
+		$session->setBeforeAuthUrl($newBeforeAuthUrl);
+		return $this;	
+	}
+		
+}
